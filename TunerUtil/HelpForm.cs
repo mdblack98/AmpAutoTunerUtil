258019c8629be2803d705c02d58bc37f3f0f789d
Michael Black <mdblack98@yahoo.com>
2019-05-13 11:37:39 -0500

    Add help draft

---
+++ TunerUtil/HelpForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmpAutoTunerUtility
+{
+    public partial class HelpForm : Form
+    {
+        public HelpForm()
+        {
+            InitializeComponent();
+        }
+
+            private void HelpForm_Load(object sender, EventArgs e)
+            {
+                String path = Assembly.GetExecutingAssembly().CodeBase;
+                String helpFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase) + "\\AmpAutoTunerUtility.htm";
+                //MessageBox.Show(helpFile);
+                webBrowser1.Url = new System.Uri(helpFile);
+            }
+
+    }
+}
