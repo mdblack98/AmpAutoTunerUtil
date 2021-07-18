7bfe7afb31c78d371d2bc329878a3fc81e6444d3
Michael Black W9MDB <mdblack98@yahoo.com>
2021-07-18 17:30:20 -0500

    v0.67

---
+++ TunerUtil/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmpAutoTunerUtility
+{
+    public static class ExtensionMethods
+    {
+        //public static void InvokeAndClose(this Control self, MethodInvoker func)
+        //{
+        //    if (self != null)
+        //    {
+        //        IAsyncResult result = self.BeginInvoke(func);
+        //        self.EndInvoke(result);
+        //        result.AsyncWaitHandle.Close();
+        //    }
+        //}
+    }
+}
